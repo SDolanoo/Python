a5a7e75cf13ba5ccf75c4dc7d815317220743f0e
Dolan <sdolazinski@gmail.com>
2024-01-29 21:03:05 +0100

    Password_manager_final_update

--- MidProjects/Password_manager/main.py
+++ MidProjects/Password_manager/main.py
@@ -1,21 +1,16 @@
 from tkinter import *
 from tkinter import messagebox
-from random import choice, randint, shuffle
+import random
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-    password_letters = [choice(letters) for _ in range(randint(8, 10))]
-    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
-
-    password_list = password_letters + password_symbols + password_numbers
-    shuffle(password_list)
-
-    password = "".join(password_list)
-    password_entry.insert(0, password)
+    letters = [i for i in "0123456789!@#$%^&*-_+=,<.>/?;:`~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+    password = []
+    for n in range(12):
+        password.append(random.choice(letters))
+    password2 = "".join(password)
+    password_entry.delete(0, END)
+    password_entry.insert(END, f"{password2}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
 
@@ -23,17 +18,53 @@ def save():
     username = username_entry.get()
     password = password_entry.get()
 
+    new_data = {
+        website: {
+            "username": username,
+            "password": password,
+        }
+    }
+
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nUsername: {username} "
-                                                      f"\nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {username} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        #is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nUsername: {username} "
+        #                                              f"\nPassword: {password} \nIs it ok to save?")
+        try:
+            with open("data.json", 'r') as file:
+                # Reading old data
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", 'w') as file:
+                # saving updated data
+                json.dump(new_data, file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
 
+            with open("data.json", 'w') as file:
+                # saving updated data
+                json.dump(data, file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+# ------------------------ BROWSE USER DATA --------------------------- #
+def browse_user_data():
+    website = (website_entry.get()).title()
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+            searched_website = data[website]
+    except FileNotFoundError:
+        messagebox.showerror(title="Hello", message="There is no data in the manager. \n"
+                                                   "First You need to add data to be able to browse it.")
+        pass
+    except KeyError:
+        messagebox.showerror(title="Hello", message="There is either: No website You try to find or You misspelled it.")
+    else:
+        username = searched_website['username']
+        password = searched_website['password']
+        messagebox.showinfo(title="Your data", message=f"Username: {username}\n Password: {password}")
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -54,16 +85,18 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=32)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-username_entry = Entry(width=35)
+username_entry = Entry(width=50)
 username_entry.grid(row=2, column=1, columnspan=2)
 username_entry.insert(0, "sdolazinski@gmail.com")
-password_entry = Entry(width=21)
+password_entry = Entry(width=32)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+browse_button = Button(text="Browse", width=14, command=browse_user_data)
+browse_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
