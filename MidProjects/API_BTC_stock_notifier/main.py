88bfa06500fd452470450a39a7d4467ff99ad6f2
Dolan <sdolazinski@gmail.com>
2024-02-14 20:57:18 +0100

    API_BTC final

--- MidProjects/API_BTC_stock_notifier/main.py
+++ MidProjects/API_BTC_stock_notifier/main.py
@@ -8,10 +8,10 @@ def get_change(current, previous):
         return 100.0
     try:
         if current > previous:
-            higher = f"+{round((abs(current - previous) / previous) * 100.0, 2)}"
+            higher = f"🔼{round((abs(current - previous) / previous) * 100.0, 2)}"
             return higher
         else:
-            lower = f"-{round((abs(current - previous) / previous) * 100.0, 2)}"
+            lower = f"🔽{round((abs(current - previous) / previous) * 100.0, 2)}"
             return lower
     except ZeroDivisionError:
         return 0
@@ -24,33 +24,32 @@ STOCK = "BTC"
 COMPANY_NAME = "Bitcoin"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-STOCK_APIKEY = "FCTKSV0BMN1LNMUX"
+STOCK_APIKEY = "TRC72ZEKT9Q3Z7CX"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 NEWS_APIKEY = "23ce1616469c4632a02571758e4054d7"
 
-MY_EMAIL="malytrolll@gmail.com"
-MY_PASSWORD="xxxxxxxxxxxxxxxxx"
+MY_EMAIL = "malytrolll@gmail.com"
+MY_PASSWORD = "xxxx"
 TO_EMAIL = "sdolazinski@gmail.com"
 
 ## Get the price and price change for BTC
 
-# alphavantage_params = {
-#     "function": "DIGITAL_CURRENCY_DAILY",
-#     "symbol": f"{STOCK}",
-#     "market": "USD",
-#     "apikey": f"{STOCK_APIKEY}"
-# }
-# response_stock_price = requests.get(url=f"{STOCK_ENDPOINT}", params=alphavantage_params)
-# response_stock_price.raise_for_status()
-#
-# data = response_stock_price.json()
-# today_close = data["Time Series (Digital Currency Daily)"][f"{TODAY}"]["4a. close (USD)"]
-#
-# yesterday_close = data["Time Series (Digital Currency Daily)"][f"{YESTERDAY}"]["4a. close (USD)"]
-#
-# percent_change = get_change(float(today_close), float(yesterday_close))
-#
-# print(f"The price change of {STOCK} was: {percent_change}")
+alphavantage_params = {
+    "function": "DIGITAL_CURRENCY_DAILY",
+    "symbol": STOCK,
+    "market": "USD",
+    "apikey": STOCK_APIKEY
+}
+response_stock_price = requests.get(url=STOCK_ENDPOINT, params=alphavantage_params)
+response_stock_price.raise_for_status()
+
+data = response_stock_price.json()
+print(data)
+today_close = data["Time Series (Digital Currency Daily)"][f"{TODAY}"]["4a. close (USD)"]
+
+yesterday_close = data["Time Series (Digital Currency Daily)"][f"{YESTERDAY}"]["4a. close (USD)"]
+
+percent_change = get_change(float(today_close), float(yesterday_close))
 
 ## Get the top trending article from https://newsapi.org/docs/endpoints/everything
 
@@ -67,6 +66,9 @@ news_params = {
 response_news = requests.get(url=NEWS_ENDPOINT, params=news_params)
 response_news.raise_for_status()
 article = response_news.json()["articles"][0]
+article_title = article['title']
+article_desc = article['description']
+article_url = article["url"]
 
 #data_news_description = response_news.json()["articles"]
 
@@ -76,11 +78,10 @@ print(f"Description: {article['description']}")
 
 ## Send an email with stock price change, actual price and top trending article
 
-# TESLA_UPDATE = f"dotdot"
-# TESLA_ARTICLE = f"dotdot"
-# message = f"Subject:Tesla stock update\n\n{TESLA_UPDATE}\n{TESLA_ARTICLE}"
-#
-# with smtplib.SMTP("smtp@gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-#     connection.sendmail(from_addr=MY_EMAIL, to_addrs=TO_EMAIL, msg=message)
+message = (f"Subject:BTC update\n\n{STOCK}: {percent_change}\nHeadline: {article_title}\nBrief: {article_desc}\n"
+           f"Url: {article_url}")
+
+with smtplib.SMTP("smtp@gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+    connection.sendmail(from_addr=MY_EMAIL, to_addrs=TO_EMAIL, msg=message)
