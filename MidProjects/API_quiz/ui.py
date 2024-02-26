c33f592b4ab1e6516035d73e013de51c7571ffa2
SDolanoo <155109768+SDolanoo@users.noreply.github.com>
2024-02-26 13:46:39 +0100

    .

--- MidProjects/API_quiz/ui.py
+++ MidProjects/API_quiz/ui.py
@@ -13,7 +13,7 @@ class QuizInterface:
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         # Canvas
         self.canvas = Canvas(height=250, width=300, bg="white")
-        self.question_text = self.canvas.create_text(150, 125, width= 280, text="Hejka piekna dziewczyno", font=("Arial", 20, "italic"))
+        self.question_text = self.canvas.create_text(150, 125, width= 280, text="Insert text", font=("Arial", 20, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, padx=30, pady=30)
         # Label
         self.score_label = Label(text="Score 0", bg=THEME_COLOR, fg="white")
